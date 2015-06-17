51f22661ce0c40edaba8c87e66d6b2e4f7a7af24
M <mgonzomatic@gmail.com>
2015-06-17 00:13:03 +0000

    First commit.
    Using gulp, sass, node, express.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// https://www.codefellows.org/blog/quick-intro-to-gulp.js
+// https://www.package/gulp-ruby-sass
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  return sass('/home/mgonzo/webapps/mgonzomatic/nodeapps/fuerzadlb/public/sass/style.scss')
+    .on('error', function (err) { 
+      console.error('Error', err.message);
+    })
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/stylesheets'))
+});
+
+gulp.task('default', ['sass']);
