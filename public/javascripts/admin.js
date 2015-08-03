2272ba223f0cdc9590bf7469a78ed4368e704e6c
mgonzo <marcos.gonzales@sheknows.com>
2015-08-02 18:18:46 -0700

    Replaced admin js with React
    Why use backbone?  React is great.

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,62 +1,44 @@
-var app = {};
-
-/*
- * Model
- */
-
-// model definition
-app.Message = Backbone.Model.extend({
-  defaults: {
-    messageId: ''
-  }
-});
-
-
-// view
-app.MessageView = Backbone.View.extend({
-  tagName: 'li',
-  className: 'message',
-  initialize: function () {
-    this.template = _.template($('#message-template').html());
+var MessageList = React.createClass({
+  getInitialState: function() {
+    return {
+      messages: []
+    }
   },
-  render: function () {
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-});
 
-
-/*
- * Collection
- */
-var MessageList = Backbone.Collection.extend({ 
-  model: app.Message,
-  url: 'http://localhost:3000/messages'
-});
-
-app.messages = new MessageList();
-
-app.AppView = Backbone.View.extend({
-  el: '#messages',
-  initialize: function () {
-    console.log('appview');
-    this.render();
-  },
-  render: function () {
-    var that = this;
-    app.messages.fetch({
-      success: function (messages) {
-        if (app.messages.length) {
-          app.messages.each(that.addOne, that);
-        }
+  componentDidMount: function() {
+    $.get(this.props.source, function (data) {
+      console.log(data);
+      if (this.isMounted()) {
+        this.setState({
+          messages: JSON.parse(data)
+        });
       }
-    });
+    }.bind(this));
   },
-  addOne: function (message) {
-    var view = new app.MessageView({model: message});
-    this.$el.append(view.render().el);
+
+  render: function() {
+    return (
+        <div>
+          <ul> { 
+            this.state.messages.map( function (item, index) { 
+              return <li>
+                <div>{item.messageId}</div>
+                <div>{item.date}</div>
+                <div>{item.fname}</div>
+                <div>{item.lname}</div>
+                <div>{item.deleted}</div>
+                <div>{item.email}</div>
+                <div>{item.phone}</div>
+                <div>{item.body}</div>
+              </li> 
+            })
+          } </ul>
+        </div>
+    );
   }
 });
 
-new app.AppView();
-
+React.render(
+    <MessageList source="http://localhost:3000/messages"/>,
+    document.getElementById('messages')
+);
